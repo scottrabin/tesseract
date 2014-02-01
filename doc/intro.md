f83d9a6964907915a3ac0419cce06f1cb336a2f6
Scott Rabin <scottrabin@gmail.com>
2014-02-01 13:17:25 -0800

    Project init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tesseract
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/tesseract/core_test.clj
@@ -0,0 +1,7 @@
+(ns tesseract.core-test
+  (:require [clojure.test :refer :all]
+            [tesseract.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
