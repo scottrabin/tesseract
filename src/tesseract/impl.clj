654c84b4805d53b612540b6db9f8382ce88842af
Scott Rabin <scottrabin@gmail.com>
2014-02-01 22:12:47 -0800

    Add implementation & tests for accepting non-string attribute values

--- src/tesseract/impl.clj
+++ src/tesseract/impl.clj
@@ -1,4 +1,6 @@
-(ns tesseract.impl)
+(ns tesseract.impl
+  (:require
+    [clojure.string]))
 
 (defprotocol IRender
   (render [item]
@@ -8,3 +10,44 @@
   String
   (render [string]
     string))
+
+(defprotocol IAttributeValue
+  (to-attr [this] "Generate an appropriate attribute value string"))
+
+(def HTML_ATTR_ESCAPE {\< "&lt;"
+                       \> "&gt;"
+                       \" "&quot;"
+                       \' "&apos;"
+                       \& "&amp;"})
+(extend-protocol IAttributeValue
+  String
+  (to-attr [this]
+    (clojure.string/escape this HTML_ATTR_ESCAPE))
+
+  clojure.lang.Keyword
+  (to-attr [this]
+    (to-attr (name this)))
+
+  clojure.lang.LazySeq
+  (to-attr [this]
+    (clojure.string/join " " (map to-attr this)))
+
+  clojure.lang.PersistentList
+  (to-attr [this]
+    (to-attr (map to-attr this)))
+
+  clojure.lang.PersistentHashSet
+  (to-attr [this]
+    (to-attr (map to-attr this)))
+
+  clojure.lang.PersistentArrayMap
+  (to-attr [this]
+    (to-attr (for [[k v] this :when v] k)))
+
+  clojure.lang.PersistentTreeMap
+  (to-attr [this]
+    (to-attr (for [[k v] this :when v] k)))
+
+  clojure.lang.PersistentTreeSet
+  (to-attr [this]
+    (to-attr (map to-attr this))))
---
+++ test/tesseract/impl_test.clj
@@ -0,0 +1,63 @@
+(ns tesseract.impl-test
+  (:require [clojure.test :refer :all]
+            [tesseract.impl :as impl]))
+
+(defn attr=
+  "Compare attribute values to check equality"
+  [& attrvals]
+  (apply = (map #(set (clojure.string/split % #"\s")) attrvals)))
+
+; TODO
+(deftest render)
+
+(deftest to-attr
+  (testing "Keywords"
+    (is (attr= "value"
+               (impl/to-attr :value))))
+  (testing "Strings"
+    (is (attr= "attribute-value"
+               (impl/to-attr "attribute-value")))
+    (is (attr= "escaping &lt; &gt; &quot; &apos; &amp;"
+               (impl/to-attr "escaping < > \" ' &"))))
+  (testing "Lists"
+    (is (attr= "first second third"
+               (impl/to-attr (list :first "second" "third")))))
+  (testing "Vectors"
+    (is (attr= "first second third"
+               (impl/to-attr ["first" "second" "third"])))
+    (is (attr= "keyword second third"
+               (impl/to-attr [:keyword "second" "third"])))
+    (is (attr= "hash-one hash-two second keyword"
+               (impl/to-attr [{:hash-one true
+                               :hash-two true}
+                              "second"
+                              :keyword]))))
+  (testing "Hash maps"
+    (is (attr= "first second"
+               (impl/to-attr {"first" true
+                              "second" true})))
+    (is (attr= "second only"
+               (impl/to-attr {"first" false
+                              "second" true
+                              "only" true})))
+    (is (attr= "vector key other"
+               (impl/to-attr {["vector" "key"] true
+                              "other" true})))
+    (is (attr= "keyword other"
+               (impl/to-attr {:keyword true
+                              "other" true}))))
+  (testing "Sets"
+    (is (attr= "first second"
+               (impl/to-attr #{:first "second"}))))
+  (testing "Sorted map"
+    ; attribute value should come out in the sorted order
+    (is (= "first second"
+           (impl/to-attr (sorted-map-by
+                           #(compare (name %1) (name %2))
+                           :first true "second" true :third false)))))
+  (testing "Sorted set"
+    ; attribute value should come out in the sorted order
+    (is (= "first second"
+           (impl/to-attr (sorted-set-by
+                           compare
+                           :first :second))))))
