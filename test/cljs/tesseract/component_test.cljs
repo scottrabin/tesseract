b65df282a478f2b7a8c23b1e8b304cdaa2e391a9
loganlinn <logan.linn@gmail.com>
2014-03-08 17:36:19 -0800

    Update tesseract.component-test
    Test the IBuiltComponent interface for associng and getting children

--- test/cljs/tesseract/component_test.cljs
+++ test/cljs/tesseract/component_test.cljs
@@ -6,42 +6,78 @@
             [tesseract.component :as component]
             [tesseract.dom :as dom]))
 
-(defcomponent NumList
+(defcomponent OrderedList
   (will-build! [this next-component])
   (did-build! [this prev-component root-node])
   (will-mount! [this] this)
   (did-mount! [this root-node])
   (render [component]
-    (dom/ul {:class :test-component}
-            (for [i (range 10)]
-              (dom/li {} (str "Number " i))))))
+          (apply dom/ol
+                 {:class :test-component}
+                 (for [child (:children component)]
+                   (dom/li {} child)))))
 
 (deftest defines-convenience-constructor
-  (is (ifn? NumList)))
+  (is (ifn? OrderedList)))
 
 (deftest satisfies-protocols
-  (let [c (NumList {})]
+  (let [c (OrderedList {})]
     (is (satisfies? component/IComponent c))
+    (is (satisfies? component/IBuiltComponent c))
     (is (satisfies? component/IWillBuild c))
     (is (satisfies? component/IDidBuild c))
     (is (satisfies? component/IWillMount c))
     (is (satisfies? component/IDidMount c))))
 
 (deftest test-render
-  (let [component (NumList {})
-        out (component/render component)]
-    (is (= out (dom/ul {:class :test-component}
-                       (for [i (range 10)]
-                         (dom/li {} (str "Number " i))))))))
+  (let [c (OrderedList {} "first" "second")]
+    (is (= (dom/ol {:class :test-component}
+                   (dom/li {} "first")
+                   (dom/li {} "second"))
+           (component/render c)))))
+
+(deftest test-IBuiltComponent-protocol
+  (let [li0 (dom/li {} "zero")
+        li1 (dom/li {} "one")
+        ol (-> (dom/ol {} li0 li1)
+               (component/assoc-child 0 li0)
+               (component/assoc-child 1 li1))
+        c (-> (OrderedList {} li0 li1)
+              (component/assoc-child 0 ol))]
+    (testing "get-children"
+      (let [children (component/get-children c)]
+        (is (vector? children))
+        (is (= [ol]))))
+    (testing "get-child"
+      (is (= ol (component/get-child c 0))))
+    (testing "get-child-in (depth:1)"
+      (is (= li0 (component/get-child-in c [0 0])))
+      (is (= li1 (component/get-child-in c [0 1]))))
+    (testing "get-child-in (depth:2)"
+      (let [parent-li (-> (dom/li {} c)
+                          (component/assoc-child 0 c))
+            parent-ol (-> (dom/ol {} parent-li)
+                          (component/assoc-child 0 parent-li))
+            parent-c (-> (OrderedList {} c)
+                         (component/assoc-child 0 parent-ol))]
+        (is (= parent-ol (component/get-child parent-c 0)))
+        (is (= parent-li (component/get-child-in parent-c [0 0])))
+        (is (= c (component/get-child-in parent-c [0 0 0])))
+        (is (= ol (component/get-child-in parent-c [0 0 0 0])))
+        (is (= li0 (component/get-child-in parent-c [0 0 0 0 0])))
+        (is (= li1 (component/get-child-in parent-c [0 0 0 0 1])))))
+    ))
+
 (deftest test-attach!
-  (let [c (NumList {})]
+  (let [c (OrderedList {} "first" "second")]
     (core/attach! c js/document.body)
     (is (= js/document.body.children.length 1))
-    (is (= js/document.body.firstChild.nodeName "UL"))
-    (is (= js/document.body.firstChild.children.length 10))
-    (doseq [i (range 10)]
-      (is (= (.-textContent (aget js/document.body.firstChild.children i))
-             (str "Number " i))))))
+    (is (= js/document.body.firstChild.nodeName "OL"))
+    (is (= js/document.body.firstChild.children.length 2))
+    (is (= "first" (.-textContent (aget js/document.body.firstChild.children 0))))
+    (is (= "second" (.-textContent (aget js/document.body.firstChild.children 1))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defcomponent Comment
   (render [{:keys [attrs children]}]
@@ -63,3 +99,4 @@
     (is (= 2 (count (:children out))))
     (is (= "<div class=\"comment-list\"><div class=\"comment\"><h2 class=\"comment-author\">Logan Linn</h2>This is one comment</div><div class=\"comment\"><h2 class=\"comment-author\">Scott Rabin</h2>This is *another* comment</div></div>"
            (str out)))))
+
